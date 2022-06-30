2c31655532d67e4804d34796912b1c1d8be51bd1
prechilala <puzoma16@yahoo.com>
2022-06-30 16:48:46 -0700

    6 Sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,6 +10,8 @@ int main(void)
 	long long int ll;
 	float d;
 	char c;
+
+
 	printf("Size of a char: %lu byte(s)\n", sizeof(c));
 	printf("Size of an int: %lu byte(s)\n", sizeof(i));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(l));
