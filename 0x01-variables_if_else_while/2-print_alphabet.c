ab99f3da81c4e6c087fb560cceb6c7ca75d07348
prechilala <puzoma16@yahoo.com>
2022-07-01 16:01:41 -0700

    alphabet game

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,11 +7,16 @@
 */
 int main(void)
 {
-	char low;
+	char c = 'a';
 
-	for (low = 'a'; low <= 'z'; low++)
-		putchar(low);
-		putchar('\n');
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+
+
+	putchar('\n');
 
 	return (0);
 
