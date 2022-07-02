1108f91189c589c80c4223b74f460f673ba310b0
prechilala <puzoma16@yahoo.com>
2022-07-02 14:32:49 -0700

    task5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,8 +2,7 @@
 
 /**
  * main - to carry out tasks
- * Description - main value
- * followed by new line
+ * 
  * Return: Always 0 (success)
  */
 int main(void)
@@ -12,7 +11,9 @@ int main(void)
 
 	for (n = 0; n < 10; n++)
 		printf("%d", n);
-		printf("\n");
+
+	printf("\n");
+
 	return (0);
 
 }
