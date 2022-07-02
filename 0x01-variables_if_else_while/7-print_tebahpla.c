28b2ce43137dfb00d347745c6cb2aece6e8fe184
prechilala <puzoma16@yahoo.com>
2022-07-02 15:24:23 -0700

    task7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - to carry out tasks
+ * Description - main value
+ * followed by new line
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char low;
+
+	for (low = 'z'; low >= 'a'; low--)
+		putchar(low);
+		putchar('\n');
+	return (0);
+
+}
