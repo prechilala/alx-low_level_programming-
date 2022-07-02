0b34fdbbab506ea8086714f3db7e690e5d5ea79b
prechilala <puzoma16@yahoo.com>
2022-07-02 15:01:58 -0700

    task6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - to carry out tasks
+ * Description - main value
+ * followed by new line
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+		putchar(n);
+	putchar('\n');
+	return (0);
+
+}
