f0dbff9ee1d605791a6f6617c0dbdc96cfd3e4bb
prechilala <puzoma16@yahoo.com>
2022-07-05 05:53:29 -0700

    task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - to carry out projects
+ * Return: always 0
+*/
+
+void print_alphabet(void);
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
+	return (0);
+}
