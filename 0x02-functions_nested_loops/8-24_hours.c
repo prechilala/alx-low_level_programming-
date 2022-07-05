0fd906ddd2be62ef25db3c3fc672ecb1b889be90
prechilala <puzoma16@yahoo.com>
2022-07-05 10:39:23 -0700

    prints 24hrs

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * jack_bauer ->  a function that prints every minute of the day
+*/
+
+void jack_bauer(void)
+{
+	int x, y;
+
+	for (x = 0; x < 24; x++)
+	{
+		for (y = 0; y < 60; y++)
+		{
+			if (x < 10)
+			{
+				_putchar('0');
+				_putchar(x + '0');
+			}
+			else if (x >= 10)
+			{
+				_putchar((x / 10) + '0');
+				_putchar((x % 10) + '0');
+			}
+			if (y < 10)
+			{
+				_putchar(':');
+				_putchar('0');
+				_putchar(y + '0');
+			}
+			else if (y >= 10)
+			{
+				_putchar(':');
+				_putchar((y / 10) + '0');
+				_putchar((j % 10) + '0');
+			}
+			_putchar('\n');
+		}
+	}
+}
