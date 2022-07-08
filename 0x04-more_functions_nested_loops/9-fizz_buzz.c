aebe232720d9954b92d1bb960bd88636f7b781a5
prechilala <puzoma16@yahoo.com>
2022-07-08 14:20:38 -0700

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * main -> this is a program to print fizzbuzz
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i =1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 != 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0 && i % 3 != 0)
+		{
+			printf("Buzz");
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i == 1)
+		{
+			printf("%d", i);
+		}
+	}
+	printf(\n);
+
+	return (0);
+}
