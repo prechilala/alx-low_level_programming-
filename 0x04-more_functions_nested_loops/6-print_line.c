57cb92e59d95394fc4fed8ec39c1e4d9da3d7be0
prechilala <puzoma16@yahoo.com>
2022-07-08 10:15:58 -0700

    print lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_line -> this is a function print a line
+ * @n: an integer parameter
+ * Return: always 0
+ */
+
+void print_line(int n);
+{
+	int x;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (x = 0; x < n; x++)
+			_putchar(95)
+		_putchar('\n')
+	}
+}
