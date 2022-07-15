62733878ea323713c303ce4675b63035606bb55f
prechilala <puzoma16@yahoo.com>
2022-07-14 18:02:35 -0700

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp -> function to compare
+ * @s1: first parameter
+ * @s2: second parmeter
+ * Return: a integer
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	int result;
+
+	result = strcmp(s1, s2);
+	return (result);
+}
